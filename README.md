6ff910d344aa5035344ed8ecfd8d281c617c3c3d
n3adams-stack <n3adams@ucsd.edu>
2026-04-25 16:44:09 -0700

    Re-adding explore page, questions, and actions pictures

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Lab 5
+
 Nicholas Adams
+
+
+
+1. No, because the message feature of a message application is most likely complex and dependent on other components, so unit testing would not be a good way to ensure this feature works properly.
+2. Yes, because checking a message length is a single function and likely not dependent on other components of the app, so unit testing would be reasonable and efficient.
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,62 @@
-// explore.js
-
 window.addEventListener('DOMContentLoaded', init);
+
+
+const synth = window.speechSynthesis;
+
+const inputForm = document.querySelector("select");
+const inputTxt = document.getElementById("text-to-speak");
+const voiceSelect = document.querySelector("select");
+
+let voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (const voice of voices) {
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+
+    if (voice.default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function play_sound() {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (const voice of voices) {
+    if (voice.name === selectedOption) {
+      utterThis.voice = voice;
+    }
+  }
+  utterThis.pitch = 1
+  utterThis.rate = 1;
+
+  let main_img = document.querySelector('img');
+  main_img.src = "assets/images/smiling-open.png";
+  utterThis.onend = function(event)  { main_img.src = "assets/images/smiling.png";};
+  synth.speak(utterThis);
+  inputTxt.blur();
+};
+
+
 
 function init() {
   // TODO
+  let main_selector = document.querySelector('select');
+  let main_button = document.querySelector('button');
+  main_button.addEventListener('click',function() {play_sound();})
 }
